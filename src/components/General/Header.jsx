0c81c6d25e5c187e9048e04a62bafd05b8e19e1c
NikoLen <web@nikolen.cz>
2024-02-15 19:45:09 +0100

    Adjust header  and remove christmas image

--- src/components/General/Header.jsx
+++ src/components/General/Header.jsx
@@ -4,9 +4,8 @@ const Header = () => {
   return (
     <header>
       <h1>
-        <a href="https://nikolen-movie-app.vercel.app">Movie app</a>
+        <a href='https://nikolen-movie-app.vercel.app'>Movie app</a>
       </h1>
-      <img src="./img/gift.png" alt="christmass gift" />
     </header>
   );
 };
